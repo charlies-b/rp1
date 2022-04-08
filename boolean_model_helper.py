801dc5d99508d2666915d73b450383a58bc5338c
Charlie Stainton-Bygrave <charlie@Charlies-MacBook-Pro.local>
2022-04-08 20:34:58 +0100

    analysis mtb on/off

--- boolean_model_helper.py
+++ boolean_model_helper.py
@@ -5,9 +5,10 @@ import warnings
 warnings.filterwarnings("ignore")
 import boolean2 as b2
 import matplotlib.pyplot as plt
+import string_model_builder_3 as builder
 from numpy.ma import masked_equal
-from matplotlib.colors import ListedColormap
-plt.rcParams.update({'font.size': 22})
+import numpy as np
+from mpl_toolkits.axes_grid1.axes_divider import make_axes_locatable
 
 def missing( node_name ): # initialise any loose nodes True
     return True
@@ -26,37 +27,64 @@ def print_model(model): # print node states
     for node in model.data:
         print node, model.data[node]
         
-def plot_model(model, nodes=None, w=10, h=32, fontsize=12): # plot node states 
-    
-    # get data from model
-    data = []
-    labels = sorted(model.data.keys()) if not nodes else nodes # sort alphabetically, or a list of ordered nodes
-    for label in labels:
-        data.append(model.data[label])
-        
-    # plot figure
-    plt.rcParams.update({'font.size': fontsize})
-    plt.rcParams['figure.figsize'] = [w, h]
-    cmap=plt.cm.get_cmap('gray') # off (0) = black, on (1) = white
-    plt.yticks(range(0, len(labels)), labels)
-    plt.imshow(data, cmap=cmap)
-    plt.show()
-
-class DummyModel: # dummy model with data attribute
-    def __init__(self, data):
-        self.data = data
-        
-def plot_average(models, nodes = None, w=10, h=32):
-    # collect models into average (from booleannet docs)
+def average_models(models, nodes = None, w=10, h=32): # average array of models (from booleannet docs)
     coll = b2.util.Collector()
     for model in models:
         coll.collect(states = model.states, nodes=model.nodes)
-    avgs = coll.get_averages()   
-    
-    # plot average model
-    avg_model = DummyModel(avgs) # so it can be passed to plot_model plain
-    plot_model(avg_model, nodes=nodes, w=w, h=h)
+    avgs = coll.get_averages()     
+    return avgs
 
+def get_cycles(models):
+    # (start index, size) 
+    # size = 0 => no cycle/steady state
+    # size = 1 => steady state
+    return [ model.detect_cycles() for model in models]
+
+def count_cycles(cycles):
+    # (# none, # steady, # other)
+    none = 0
+    steady = 0
+    other = 0 
+    for cycle in cycles:
+        if cycle[1] == 0: none+=1
+        elif cycle[1] == 1: steady+=1
+        else: other+=1
+    return (none, steady, other)
+
+def print_cycles(cycles):
+    print "index", '\t', 'size'
+    for cycle in cycles: 
+        print cycle[0], '\t', cycle[1]   
+
+def average_cycles(cycles):
+    return np.average(cycles, 0)
+        
+def plot_data(data, nodes=None, w=10, h=5, fontsize=12, title = 'Node Heatmap'): # plot model.data (node states) 
+    plt.rcParams.update({'font.size': fontsize})
+
+    # set figure
+    fig, ax = plt.subplots(1,1, figsize=(w,h))
+
+    # select plot data
+    plot = []
+    labels = sorted(data.keys()) if not nodes else nodes # sort alphabetically, or a list of ordered nodes
+    for label in labels:
+        bindata = np.array(data[label]).astype(float) # convert bool to bin if necessary
+        plot.append(bindata)
+        
+    # plot data
+    cmap=plt.cm.get_cmap('gray') # off (0) = black, on (1) = white
+    im = ax.imshow(plot, cmap=cmap)
+    ax.set_yticks(np.arange(len(labels)))
+    ax.set_yticklabels(labels)
+    ax.set_aspect('auto')
+    ax.set_title(title)
+    ax.set_xlabel('steps')
+    
+    divider = make_axes_locatable(ax) # adding color bar here ...
+    cax = divider.append_axes('right', size='2%', pad='1%')
+    fig.colorbar(im, orientation = 'vertical', cax= cax) 
+        
 def knockout(definition, knockouts=[]):
     assert isinstance(knockouts, list), "takes list"
     new_definition = definition.split('\n')
